6a6fbe95d85559efd7a141cc697a3c7c2dbb55c0
singularitti <singularitti@outlook.com>
2024-09-28 18:32:17 -0600

    Add `gershgorin_extrema`

--- src/GershgorinDiscs.jl
+++ src/GershgorinDiscs.jl
@@ -1,5 +1,22 @@
 module GershgorinDiscs
 
-# Write your package code here.
+using LinearAlgebra: diag
+
+export gershgorin_extrema
+
+function gershgorin_extrema(A::AbstractMatrix)
+    λₘᵢₙ, λₘₐₓ = zero(eltype(A)), zero(eltype(A))
+    centers = diag(A)
+    for (row, center) in zip(eachrow(A), centers)
+        radius = sum(abs, row) - abs(center)
+        left, right = center - radius, center + radius
+        if left < λₘᵢₙ
+            λₘᵢₙ = left
+        elseif right > λₘₐₓ
+            λₘₐₓ = right
+        end
+    end
+    return λₘᵢₙ, λₘₐₓ
+end
 
 end
